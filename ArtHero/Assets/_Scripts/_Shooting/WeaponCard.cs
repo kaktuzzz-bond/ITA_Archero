f413e58224f58be0ad2c9620014bcd1ed82c567e
Nicko <nickolay.321@gmail.com>
2023-04-10 23:59:18 +0300

    commit shooting

---
+++ ArtHero/Assets/_Scripts/_Shooting/WeaponCard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "ArcHero/Weapon")]
+public class WeaponCard : ScriptableObject
+{
+    public Transform weaponPrefab;
+
+    [Header("Weapon Setup")]
+    public float speed;
+
+    public float damage;
+
+    public int hitTimes; //при контакте с противником летит насквозь
+
+    public bool flyOverObstacles;
+
+    public Vector3[] fraction; //если только один, ставим Vector3.forward
+
+    [Header("Weapon Sound")]
+    public Transform onlaunchVFXPrefab;
+
+    public AudioClip onRicochetVFXPrefab;
+
+    public AudioClip onHitVFXPrefab;
+
+    [Header("Weapon Sound")]
+    public AudioClip onLaunchSound;
+
+    public AudioClip onRicochetSound;
+
+    public AudioClip onHitSound;
+}
