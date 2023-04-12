c68e2b55c0adbbc9e0a3d73312b4e8fe012612c8
Nicko <nickolay.321@gmail.com>
2023-04-12 08:39:37 +0300

    commit shooting

---
+++ ArtHero/Assets/_Scripts/_Shooting/Arrow.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Arrow : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+    }
+}
