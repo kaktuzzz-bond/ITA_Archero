cf11eeeb4d215a80df95a874814a84dbf28564fd
Anton Bandarenka <kaktuzzz.b@gmail.com>
2023-04-12 20:37:50 +0300

    progressbar updated

--- ArtHero/Assets/_Scripts/Progressbar.cs
+++ ArtHero/Assets/_Scripts/Progressbar.cs
@@ -9,30 +9,6 @@ public class Progressbar : MonoBehaviour
     [SerializeField]
     private TextMeshPro healthValue;
 
-    private int _maxHealthValue = 100;
-
-    [Range(0, 100)]
-    public int playerHealth = 50;
-
-    private const float _smoothTime = 0.2f;
-
-    public int HealthValue
-    {
-        set => StartCoroutine(ChangeHealthValueRoutine(value));
-    }
-
-    private void Start()
-    {
-        Invoke(nameof(Test), 3f);
-    }
-
-
-
-    private void Test()
-    {
-        HealthValue = 0;
-    }
-    
     [SerializeField]
     private SpriteRenderer progressbar;
 
@@ -42,25 +18,16 @@ public class Progressbar : MonoBehaviour
     {
         _maxProgressbarWidth = progressbar.size.x;
     }
-
-    private IEnumerator ChangeHealthValueRoutine(int targetValue)
+    
+    public void UpdateProgressbar(int healthPoints)
     {
-        int currentHealth = playerHealth;
+        float scaledValue = Mathf.InverseLerp(0, PlayerManager.Instance.Health, healthPoints);
         
-        while (playerHealth > targetValue)
-        {
-            float smoothValue = Mathf.Lerp(currentHealth, targetValue, _smoothTime * Time.deltaTime);
-            
-            float scaledValue = Mathf.InverseLerp(0, _maxHealthValue, smoothValue);
-
-            progressbar.size = new Vector2(_maxProgressbarWidth * scaledValue, 1f);
-
-            healthValue.text = targetValue.ToString();
-
-            yield return null;
-        }
-      
+        progressbar.size = new Vector2(_maxProgressbarWidth * scaledValue, 1f);
     }
-
-    public void SetupMaxHealthValue(int health) => _maxHealthValue = health;
+    
+    public void UpdateText(int healthPoints)
+    {
+        healthValue.text = healthPoints.ToString();
+    }
 }
--- ArtHero/Assets/_Scripts/_Managers/PlayerManager.cs
+++ ArtHero/Assets/_Scripts/_Managers/PlayerManager.cs
@@ -1,26 +1,75 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerManager : Singleton<PlayerManager>
 {
+    [Header("Setup")]
     [SerializeField]
     public Transform playerPrefab;
-
-    //на попозже:
-    //здесь будем хранить атрибуты игрока - его усиления и эффекты
-    //private int attributes = 0;
-    //тут его уровень здоровья будет
-    //public int Health{get; private set;}
     
+    public int MaxPlayerHealth { get; private set; } = 100;
+
+    private Action<int> _healthValueChangedCallback;
+
+    private Progressbar _progressbar;
+
+    private Transform _player;
+
+    private int _startHealth;
+
+    private int _currentHealth;
+
+    public int Health
+    {
+        get => _currentHealth;
+
+        private set
+        {
+            _currentHealth = value;
+
+            _healthValueChangedCallback.Invoke(_currentHealth);
+        }
+    }
+
+    private void Start()
+    {
+        _startHealth = MaxPlayerHealth;
+        
+        Invoke(nameof(TestProgress), 3f);
+    }
+
+    private void TestProgress()
+    {
+        Health -= 30;
+        
+        Invoke(nameof(TestProgress2), 3f);
+    }
+    
+    private void TestProgress2()
+    {
+        Health += 10;
+    }
+
     private void CreatePlayerIn(Vector3 origin, int width, int height)
     {
         Vector3 position = origin + new Vector3(width * 0.5f, 0.5f, 0);
 
-        Transform player = Instantiate(playerPrefab, position, Quaternion.identity, transform);
+        _player = Instantiate(playerPrefab, position, Quaternion.identity, transform);
 
-        Observer.Instance.OnPlayerCreatedNotify(player);
+        _progressbar = _player.GetComponentInChildren<Progressbar>();
+
+        if (_progressbar == null)
+            Debug.LogError("Progressbar is not found");
+
+        _healthValueChangedCallback += _progressbar.UpdateProgressbar;
+
+        _healthValueChangedCallback += _progressbar.UpdateText;
+
+        Health = _startHealth;
+
+        Observer.Instance.OnPlayerCreatedNotify(_player);
     }
-
 
     #region ENABLE / DISABLE
 
@@ -35,5 +84,4 @@ public class PlayerManager : Singleton<PlayerManager>
     }
 
     #endregion
-  
 }
